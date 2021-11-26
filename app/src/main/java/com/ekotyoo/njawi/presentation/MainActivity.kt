3e04257f9a8be2bbd0b4c8e1996ec6adc21bfde6
ekotyoo <ekeprasetyo@gmail.com>
2021-11-26 08:50:13 +0700

    Add bottom navigation bar component

--- app/src/main/java/com/ekotyoo/njawi/presentation/MainActivity.kt
+++ app/src/main/java/com/ekotyoo/njawi/presentation/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.ekotyoo.njawi.presentation
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.ekotyoo.njawi.R
 import com.ekotyoo.njawi.presentation.theme.NjawiTheme
 
 class MainActivity : ComponentActivity() {
-    lateinit var navController: NavHostController
+    private lateinit var navController: NavHostController
+
+    private lateinit var mediaPlayer: MediaPlayer
 
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mediaPlayer = MediaPlayer.create(this, R.raw.quiz)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+
 
         setContent {
             NjawiTheme {
@@ -21,7 +29,11 @@ class MainActivity : ComponentActivity() {
                 SetupNavGraph(navController = navController)
             }
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
     }
 }
 
