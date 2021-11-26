b90ad0fb6a4145bd5d32921d53f2496261886c14
vrizas <vrizasizza@gmail.com>
2021-11-26 17:43:12 +0700

    Membuat splash screen dan melakukan import logo.

--- app/src/main/java/com/ekotyoo/njawi/presentation/MainActivity.kt
+++ app/src/main/java/com/ekotyoo/njawi/presentation/MainActivity.kt
@@ -1,33 +1,101 @@
 package com.ekotyoo.njawi.presentation
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.estimateAnimationDurationMillis
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.ekotyoo.njawi.R
 import com.ekotyoo.njawi.domain.models.Question
 import com.ekotyoo.njawi.domain.models.Quiz
 import com.ekotyoo.njawi.presentation.quiz.PlayQuizScreen
 import com.ekotyoo.njawi.presentation.theme.Blue
 import com.ekotyoo.njawi.presentation.theme.NjawiTheme
 import com.ekotyoo.njawi.presentation.theme.Red
+import kotlinx.coroutines.delay
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PlayQuizScreen()
+            Surface(color = MaterialTheme.colors.background) {
+                Navigation()
+            }
+//            PlayQuizScreen()
         }
+    }
+}
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen") {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+        composable("main_screen") {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "MAIN SCREEN")
+            }
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue =  0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .scale(scale.value)
+                .fillMaxWidth(0.5f)
+            ,
+        )
     }
 }
 
--- app/src/main/java/com/ekotyoo/njawi/presentation/landing/LandingScreen.kt
+++ app/src/main/java/com/ekotyoo/njawi/presentation/landing/LandingScreen.kt
@@ -1,4 +1,45 @@
 package com.ekotyoo.njawi.presentation.landing
 
-class LandingScreen {
+import android.media.Image
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ekotyoo.njawi.presentation.theme.NjawiTheme
+
+class LandingScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NjawiTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Vrizas")
+                    Greeting("Izza")
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NjawiTheme {
+        Column {
+            Greeting("Vrizas")
+            Greeting("Izza")
+        }
+    }
 }
