f926f21d5875862dab22680d20b764261a31493c
MRifqiFz <mrifqi389@gmail.com>
2021-12-01 00:55:20 +0700

    Minor update

--- app/src/main/java/com/ekotyoo/njawi/common/BottomBarScreen.kt
+++ app/src/main/java/com/ekotyoo/njawi/common/BottomBarScreen.kt
@@ -1,11 +1,15 @@
 package com.ekotyoo.njawi.common
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.Gamepad
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.ekotyoo.njawi.common.navigation.Screen
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.Book
+import compose.icons.fontawesomeicons.solid.Gamepad
 
 sealed class BottomBarScreen(
     val route: String,
@@ -15,13 +19,13 @@ sealed class BottomBarScreen(
     object Quiz : BottomBarScreen(
         route = Screen.Quiz.route,
         title = "Quiz",
-        icon = Icons.Default.Face
+        icon = Icons.Default.Gamepad
     )
 
     object Belajar : BottomBarScreen(
         route = Screen.Belajar.route,
         title = "Belajar",
-        icon = Icons.Default.Home
+        icon = Icons.Default.Book
     )
 
     object Profile : BottomBarScreen(
