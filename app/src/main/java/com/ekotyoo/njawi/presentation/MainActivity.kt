192a5e2958d82109794a4f38ffbe8c6a6366cb00
vrizas <vrizasizza@gmail.com>
2021-11-30 20:39:58 +0700

    Membuat landing page

--- app/src/main/java/com/ekotyoo/njawi/presentation/MainActivity.kt
+++ app/src/main/java/com/ekotyoo/njawi/presentation/MainActivity.kt
@@ -6,17 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.compose.rememberNavController
 import com.ekotyoo.njawi.presentation.auth.LoginScreen
+import com.ekotyoo.njawi.presentation.landing.LandingScreen
 import com.ekotyoo.njawi.presentation.theme.NjawiTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
 
 class MainActivity : ComponentActivity() {
 
+    @ExperimentalPagerApi
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             NjawiTheme {
-                LoginScreen(rememberNavController())
+//                LoginScreen(rememberNavController())
+                LandingScreen()
             }
         }
     }
--- app/src/main/java/com/ekotyoo/njawi/presentation/belajar/BelajarChapterScreen.kt
+++ app/src/main/java/com/ekotyoo/njawi/presentation/belajar/BelajarChapterScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
